547535e87c42afbbd330ba89f7df1c40f13fff48
onlinestochasticmatching <139255399+onlinestochasticmatching@users.noreply.github.com>
2023-07-12 14:22:15 -0700

    Update edgeWeightedFinalBound.py

--- edgeWeightedFinalBound.py
+++ edgeWeightedFinalBound.py
@@ -7,8 +7,18 @@ def g(x,theta):
 def bound(x,epsilon,delta,theta):
 	return 1- g(x*(1-epsilon),theta)*math.exp(-(1+delta)*(1-x))
 
-N = 1000000
-epsilon = 0.11
-delta = 0.18
-theta_hat = (1-epsilon) * delta/(delta + epsilon)
-print(min([bound(x,epsilon, delta,theta_hat) for x in np.linspace(0,1,N)]))
+N = 10000
+
+bestEps = -1
+bestDelta = -1
+bestBound = 0
+for epsilon in np.linspace(0.01, 1, 100):
+	for delta in np.linspace(0.01, 1, 100):
+		theta_hat = (1-epsilon) * delta/(delta + epsilon)
+		M = min([bound(x, epsilon, delta, theta_hat) for x in np.linspace(0,1,N)])
+		if M > bestBound:
+			bestBound = M
+			bestEps = epsilon
+			bestDelta = delta
+
+print(bestBound, bestEps, bestDelta)
